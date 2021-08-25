ad8c0172278d088667c882cc262cce0a74ce7fd3
pavansidam656 <57409637+pavansidam656@users.noreply.github.com>
2021-08-25 18:42:51 +0530

    Input output code for file added

--- hello.cpp
+++ hello.cpp
@@ -2,4 +2,9 @@
 using namespace std;
 int main(){
     cout<<"Hello World!";
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
 }
