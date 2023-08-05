589d0ebd01198c564fe79df8fc07a6b47c8dcff4
Pavan A. Sidam <57409637+pavansidam656@users.noreply.github.com>
2023-08-06 01:35:19 +0530

    Time: 25 ms (87.50%), Space: 25.6 MB (26.13%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    void rotateHelper(vector<int>& nums, int start, int end){
-        while(start<end){
-            swap(nums[start],nums[end]);
-            start++;
-            end--;
-        }
-    }
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-        k=k%n;
-        rotateHelper(nums,0,n-1);
-        rotateHelper(nums,0,k-1);
-        rotateHelper(nums,k,n-1);
+        if(n<=0 || k <=0)
+            return;
+        vector<int> v(n);
+        for(int i=0;i<n;i++){
+            v[i]=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            nums[(i+k)%n]=v[i];
+        }
     }
 };
