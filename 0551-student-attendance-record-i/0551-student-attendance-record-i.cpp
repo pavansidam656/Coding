bf9be115d81da3463d91234e2626ee5385743937
Pavan A. Sidam <57409637+pavansidam656@users.noreply.github.com>
2023-08-19 01:20:49 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (43.67%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     bool checkRecord(string s) {
-        int countA = 0;
-        int countL = 0; // Keep track of consecutive 'L' characters
+        int aCount = 0, lCount = 0;
         
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == 'A') {
-                countA++;
-                countL = 0; // Reset count of consecutive 'L' characters
-            } else if (s[i] == 'L') {
-                countL++;
-                if (countL >= 3) {
-                    return false; // More than two consecutive 'L' characters
-                }
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'L') {
+                lCount++;
+                if (lCount > 2)
+                    return false;
             } else {
-                countL = 0; // Reset count of consecutive 'L' characters
-            }
-            
-            if (countA > 1) {
-                return false; // More than one 'A' found
+                lCount = 0;
+                if (s[i] == 'A')
+                    aCount++;
+                if (aCount > 1)
+                    return false;
             }
         }
         
