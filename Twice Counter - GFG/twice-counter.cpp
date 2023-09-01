8c0f20f5c3743a23f1a50ed6272c72418ee87241
Pavan A. Sidam <57409637+pavansidam656@users.noreply.github.com>
2023-09-02 03:58:18 +0530

    Added solution - LeetHub

--- Twice Counter - GFG/twice-counter.cpp
+++ Twice Counter - GFG/twice-counter.cpp
@@ -10,7 +10,7 @@ class Solution
         int countWords(string list[], int n)
         {
            //code here.
-            map<string, int> m;
+            unordered_map<string, int> m;
             for(int i=0;i<n;i++){
                 m[list[i]]++;
             }
