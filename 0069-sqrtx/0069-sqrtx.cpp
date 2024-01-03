63680292b6bd9ed10a99c991175326378c367705
Pavan A. Sidam <57409637+pavansidam656@users.noreply.github.com>
2024-01-03 19:17:07 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (15.84%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0)return 0;
-        int ans=1;
-        for(int i=1;i<=(x/i);i++){
-            ans=i;
+        long long low=1,high=x,ans=0;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+           long long sqr=mid*mid;
+            if(sqr==x){
+                return mid;
+            }
+            else if(sqr<x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
         return ans;
     }
