d949a2ddca1fe4d3e97135555e29c50b59c66bae
Pavan A. Sidam <57409637+pavansidam656@users.noreply.github.com>
2024-02-11 00:20:02 +0530

    Time: 7 ms (10.98%), Space: 8.4 MB (14.13%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-     int findInMountainArray(int target, MountainArray A) {
-        int n = A.length(), l, r, m, peak = 0;
-        // find index of peak
-        l  = 0;
-        r = n - 1;
-        while (l < r) {
-            m = (l + r) / 2;
-            if (A.get(m) < A.get(m + 1))
-                l = peak = m + 1;
-            else
-                r = m;
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int length = mountainArr.length(), peakIndex=0;
+        int start=0,end=length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(mountainArr.get(mid) < mountainArr.get(mid + 1)){
+                start=peakIndex=mid+1;
+            }
+            else{
+                end=mid;
+            }
         }
-        // find target in the left of peak
-        l = 0;
-        r = peak;
-        while (l <= r) {
-            m = (l + r) / 2;
-            if (A.get(m) < target)
-                l = m + 1;
-            else if (A.get(m) > target)
-                r = m - 1;
-            else
-                return m;
+        start=0,end=peakIndex;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mountainArr.get(mid)==target) return mid;
+            else if(mountainArr.get(mid)<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
         }
-        // find target in the right of peak
-        l = peak;
-        r = n - 1;
-        while (l <= r) {
-            m = (l + r) / 2;
-            if (A.get(m) > target)
-                l = m + 1;
-            else if (A.get(m) < target)
-                r = m - 1;
-            else
-                return m;
+        start=peakIndex,end=length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mountainArr.get(mid)==target) return mid;
+            else if(mountainArr.get(mid)>target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
         }
         return -1;
     }
